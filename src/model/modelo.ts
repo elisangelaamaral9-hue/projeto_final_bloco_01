13a15e1ef8481622b91be43990ee24187a834b0e
Elisangela Amaral <elisangela.amaral9728@gmail.com>
2025-11-13 15:14:31 -0300

    Update modelo.ts

---
+++ src/model/modelo.ts
@@ -0,0 +1,4 @@
+import { Acessorio } from "./model/Acessorio" 
+
+const pulseira = new Acessorio(1,"Pulseira de diamante", 89.9, 10, "Colar") 
+const relogio = new Acessorio(2, "Relógio Feminino", 199.9, 5, "Brinco") pulseira.visualizar() relogio.visualizar()

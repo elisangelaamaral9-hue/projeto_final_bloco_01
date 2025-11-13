a4ce8d77e04b2be1774a88a7ff3a09dd38659285
Elisangela Amaral <elisangela.amaral9728@gmail.com>
2025-11-13 15:15:47 -0300

    Update Acessorio.ts

--- src/model/Acessorio.ts
+++ src/model/Acessorio.ts
@@ -1,37 +1,30 @@
-// src/model/Acessorio.ts
 import { Produto } from "./Produto"
 
 export class Acessorio extends Produto {
-  private _tipo: string
+  private _modelo: string;
 
-  constructor(
-    id: number,
-    nome: string,
-    preco: number,
-    quantidade: number,
-    tipo: string
-  ) {
-    super(id, nome, preco, quantidade)
-    this._tipo = tipo
+  constructor(modelo: string, etiqueta: number, nome: string, preco: number, quantidade: number) {
+    super(etiqueta, nome, preco, quantidade)
+    this._modelo = modelo
   }
 
-  public get tipo(): string {
-    return this._tipo
+  public get modelo(): string {
+    return this._modelo
   }
 
-  public set tipo(tipo: string) {
-    this._tipo = tipo
+  public set modelo(modelo: string) {
+    this._modelo = modelo
   }
 
-  // Implementação do método abstrato
+ //Visualizacao
   public visualizar(): void {
     console.log("\n--------------------------------------")
-    console.log("📦 Produto: Acessório")
-    console.log(`ID: ${this.id}`)
+    console.log("Informações do Produto")
+    console.log(`ID: ${this.etiqueta}`)
     console.log(`Nome: ${this.nome}`)
     console.log(`Preço: R$ ${this.preco.toFixed(2)}`)
     console.log(`Quantidade: ${this.quantidade}`)
-    console.log(`Tipo: ${this.tipo}`)
+    console.log(`Tipo: ${this.modelo}`)
     console.log("--------------------------------------")
   }
 }
